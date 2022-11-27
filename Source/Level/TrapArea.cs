fd2b9f69c9ee21470d3d05a73ad08ef0a3166aba
Sam Holder <samholder@live.co.uk>
2022-11-27 14:25:01 +0000

    add trap entity.

---
+++ Source/Level/TrapArea.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class TrapArea : Area3D
+{
+    public override void _Ready()
+    {
+        BodyEntered += OnEntered;
+    }
+
+    private void OnEntered(Node3D body)
+    {
+        if (body.Name.ToString().StartsWith("player", StringComparison.InvariantCultureIgnoreCase))
+        {
+            body.GetNode<PlayerHealth>("./PlayerHealth")
+                .Damage(10);
+            ((CharacterBody3D)body).Velocity = Vector3.Up * 5;
+        }
+    }
+}
