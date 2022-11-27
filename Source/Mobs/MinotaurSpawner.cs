e2a19ee03e29f6175ce104bd7eb2e78bb871ac6c
Sam Holder <samholder@live.co.uk>
2022-11-27 00:11:59 +0000

    added minotaur spawners.

--- Source/Mobs/MinotaurSpawner.cs
+++ Source/Mobs/MinotaurSpawner.cs
@@ -3,7 +3,10 @@ using System;
 
 public partial class MinotaurSpawner : Node3D
 {
+	private static readonly Random Random = new Random();
 	private PackedScene _spawnScene;
+
+	[Export] public float InitialSpawnChance { get; set; } = 1.0f;
 
 	public override void _Ready()
 	{
@@ -11,13 +14,12 @@ public partial class MinotaurSpawner : Node3D
 		CallDeferred("Spawn");
     }
 
-	public override void _Process(double delta)
-	{
-
-	}
-
 	public void Spawn()
 	{
+		if (Random.NextDouble() > InitialSpawnChance)
+		{
+			return;
+		}
 		var node = (Node3D)_spawnScene.Instantiate();
 		node.Position = GlobalPosition;
 		GetTree().Root
