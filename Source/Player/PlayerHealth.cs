0c589c1454bf4b2db7132d0a8d6291661674d2cc
Sam Holder <samholder@live.co.uk>
2022-11-27 16:07:23 +0000

    added player healing.

--- Source/Player/PlayerHealth.cs
+++ Source/Player/PlayerHealth.cs
@@ -3,15 +3,16 @@ using System;
 
 public partial class PlayerHealth : Node
 {
+    private Random random = new Random();
     private DeathHandler _deathHandler;
-    private AnimationPlayer _effectAnimationPlayer;
+    private TextureRect _damageEffect;
 
-    public float Health { get; private set;  } = 100f;
+    public float Health { get; private set; } = 100f;
 
     public override void _Ready()
     {
         _deathHandler = GetParent().GetNode<DeathHandler>("./DeathHandler");
-        _effectAnimationPlayer = GetParent().GetNode<AnimationPlayer>("./Head/Camera3d/DamageOverlay/AnimationPlayer");
+        _damageEffect = GetParent().GetNode<TextureRect>("./Head/Camera3d/DamageOverlay/TextureRect");
     }
 
     public void Damage(float damage)
@@ -27,10 +28,24 @@ public partial class PlayerHealth : Node
         {
             _deathHandler.Die();
         }
-        if (Health < 100f)
+        UpdateOverlay();
+        GD.Print(Health);
+    }
+
+    public override void _Process(double delta)
+    {
+        Health += (float)(3.0 * delta);
+        Health = Math.Min(Health, 100);
+        if (random.Next(10) == 0)
         {
-            _effectAnimationPlayer.Play("FadeIn");
+            UpdateOverlay();
         }
+    }
+
+
+    private void UpdateOverlay()
+    {
+        _damageEffect.SelfModulate = new Color(1f, 1f, 1f, (100 - Health) / 100f);
     }
 
 }
